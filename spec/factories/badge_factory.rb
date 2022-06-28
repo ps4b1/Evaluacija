c6097c7cb8ecc6bb1b8950f72f99d94acdfec7fb
PetarSabolek <psabolek1705@gmail.com>
2022-06-28 13:29:01 +0200

    Final commit

--- spec/factories/badge_factory.rb
+++ spec/factories/badge_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :badge, class: Spree::Badge do
     sequence(:id) { |n| n }
-    sequence(:name) { |n| "User+#{n}" }
+    sequence(:name) { |n| "Badge#{n}" }
   end
 end
